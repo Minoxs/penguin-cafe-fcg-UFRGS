884e9d251cf22a88050a0584a1e29faae57b61e1
Mino <gufrgs2018@gmail.com>
2022-09-07 20:46:24 -0300

    Use switch statement in KeyCallback

--- src/imp/player/input.cpp
+++ src/imp/player/input.cpp
@@ -163,76 +163,76 @@ void ScrollCallback(GLFWwindow *window, double xoffset, double yoffset) {
 // Definição da função que será chamada sempre que o usuário pressionar alguma
 // tecla do teclado. Veja http://www.glfw.org/docs/latest/input_guide.html#input_key
 void KeyCallback(GLFWwindow *window, int key, int scancode, int action, int mod) {
-	// ================
-	// Não modifique este loop! Ele é utilizando para correção automatizada dos
-	// laboratórios. Deve ser sempre o primeiro comando desta função KeyCallback().
-	for (int i = 0; i < 10; ++i)
-		if (key == GLFW_KEY_0 + i && action == GLFW_PRESS && mod == GLFW_MOD_SHIFT)
-			std::exit(100 + i);
-	// ================
+	const float delta = 3.141592 / 16; // 22.5 graus, em radianos.
 
-	// Se o usuário pressionar a tecla ESC, fechamos a janela.
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
+	switch (key) {
+		case GLFW_KEY_ESCAPE:
+			if (action == GLFW_PRESS) glfwSetWindowShouldClose(window, GL_TRUE);
+			return;
 
-	// O código abaixo implementa a seguinte lógica:
-	//   Se apertar tecla X       então g_AngleX += delta;
-	//   Se apertar tecla shift+X então g_AngleX -= delta;
-	//   Se apertar tecla Y       então g_AngleY += delta;
-	//   Se apertar tecla shift+Y então g_AngleY -= delta;
-	//   Se apertar tecla Z       então g_AngleZ += delta;
-	//   Se apertar tecla shift+Z então g_AngleZ -= delta;
+		case GLFW_KEY_X:
+			if (action == GLFW_PRESS) g_AngleX += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
+			return;
 
-	float delta = 3.141592 / 16; // 22.5 graus, em radianos.
+		case GLFW_KEY_Y:
+			if (action == GLFW_PRESS) g_AngleY += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
+			return;
 
-	if (key == GLFW_KEY_X && action == GLFW_PRESS) {
-		g_AngleX += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
+		case GLFW_KEY_Z:
+			if (action == GLFW_PRESS) g_AngleZ += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
+			return;
+
+		case GLFW_KEY_SPACE:
+			if (action == GLFW_PRESS) {
+				g_AngleX = 0.0f;
+				g_AngleY = 0.0f;
+				g_AngleZ = 0.0f;
+				g_ForearmAngleX = 0.0f;
+				g_ForearmAngleZ = 0.0f;
+				g_TorsoPositionX = 0.0f;
+				g_TorsoPositionY = 0.0f;
+			}
+			return;
+
+		case GLFW_KEY_P:
+			if (action == GLFW_PRESS) g_UsePerspectiveProjection = true;
+			return;
+
+		case GLFW_KEY_O:
+			if (action == GLFW_PRESS) g_UsePerspectiveProjection = false;
+			return;
+
+		case GLFW_KEY_H:
+			if (action == GLFW_PRESS) g_ShowInfoText = !g_ShowInfoText;
+			return;
+
+		case GLFW_KEY_R:
+			if (action == GLFW_PRESS) {
+				LoadShadersFromFiles();
+				fprintf(stdout, "Shaders recarregados!\n");
+				fflush(stdout);
+			}
+			return;
+
+		case GLFW_KEY_W:
+			g_isWPressed = (action != GLFW_RELEASE);
+			return;
+
+		case GLFW_KEY_S:
+			g_isSPressed = (action != GLFW_RELEASE);
+			return;
+
+		case GLFW_KEY_A:
+			g_isAPressed = (action != GLFW_RELEASE);
+			return;
+
+		case GLFW_KEY_D:
+			g_isDPressed = (action != GLFW_RELEASE);
+			return;
+
+		default:
+			return;
 	}
-
-	if (key == GLFW_KEY_Y && action == GLFW_PRESS) {
-		g_AngleY += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
-	}
-	if (key == GLFW_KEY_Z && action == GLFW_PRESS) {
-		g_AngleZ += (mod & GLFW_MOD_SHIFT) ? -delta : delta;
-	}
-
-	// Se o usuário apertar a tecla espaço, resetamos os ângulos de Euler para zero.
-	if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
-		g_AngleX = 0.0f;
-		g_AngleY = 0.0f;
-		g_AngleZ = 0.0f;
-		g_ForearmAngleX = 0.0f;
-		g_ForearmAngleZ = 0.0f;
-		g_TorsoPositionX = 0.0f;
-		g_TorsoPositionY = 0.0f;
-	}
-
-	// Se o usuário apertar a tecla P, utilizamos projeção perspectiva.
-	if (key == GLFW_KEY_P && action == GLFW_PRESS) {
-		g_UsePerspectiveProjection = true;
-	}
-
-	// Se o usuário apertar a tecla O, utilizamos projeção ortográfica.
-	if (key == GLFW_KEY_O && action == GLFW_PRESS) {
-		g_UsePerspectiveProjection = false;
-	}
-
-	// Se o usuário apertar a tecla H, fazemos um "toggle" do texto informativo mostrado na tela.
-	if (key == GLFW_KEY_H && action == GLFW_PRESS) {
-		g_ShowInfoText = !g_ShowInfoText;
-	}
-
-	// Se o usuário apertar a tecla R, recarregamos os shaders dos arquivos "shader_fragment.glsl" e "shader_vertex.glsl".
-	if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-		LoadShadersFromFiles();
-		fprintf(stdout, "Shaders recarregados!\n");
-		fflush(stdout);
-	}
-
-    if (key == GLFW_KEY_W) g_isWPressed = (action != GLFW_RELEASE);
-    if (key == GLFW_KEY_S) g_isSPressed = (action != GLFW_RELEASE);
-    if (key == GLFW_KEY_A) g_isAPressed = (action != GLFW_RELEASE);
-    if (key == GLFW_KEY_D) g_isDPressed = (action != GLFW_RELEASE);
 }
 
 // Definimos o callback para impressão de erros da GLFW no terminal
